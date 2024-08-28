de266789f32fd447fd9e7896a02441beb69452af
yastsc <yaswitha.n04@gmail.com>
2024-08-29 01:15:03 +0800

    Add date formatting for Event and Deadline tasks

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,4 +1,8 @@
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.Objects;
 
 public class Event extends Task {
     public Event(String description) {
@@ -9,9 +13,17 @@ public class Event extends Task {
         String[] strArray = super.getDescription().split("/");
         String[] startArr = strArray[1].split(" ", 2);
         String startRes = (startArr.length > 1) ? startArr[1] : "";
-        String[] endArr = strArray[2].split(" ", 2);
-        String endRes = (endArr.length > 1) ? endArr[1] : "";
-        return "(from: "  + startRes + "to: " + endRes + ")";
+        String[] endArr = (strArray.length > 2) ? strArray[2].split(" ", 2) : null;
+        String endRes = ((endArr != null)) ? (endArr.length > 2) ? endArr[1] : "" : "";
+        LocalDate d1 = null;
+        DateTimeFormatter formatterDate = new DateTimeFormatterBuilder()
+                .parseCaseInsensitive()
+                .appendPattern("yyyy-MM-dd").toFormatter();
+        if (startRes.contains("-")) {
+            d1 = LocalDate.parse(startRes, formatterDate);
+        }
+        return "(from: "  + ((d1 != null) ? d1.format(DateTimeFormatter.ofPattern("MMM d yyyy")) : startRes) +
+                ((!Objects.equals(endRes, "")) ? " to: " + endRes + ")" : ")");
     }
 
     public String getTask() {
