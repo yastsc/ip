a4679542c743155df2566cc0140789c7b184d832
yastsc <yaswitha.n04@gmail.com>
2024-08-30 02:33:02 +0800

    Add relevant methods to Storage class

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,2 +1,72 @@
+import java.io.*;
+import java.util.ArrayList;
+
 public class Storage {
+    private String filePath;
+
+    public Storage(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public ArrayList<Task> load() throws StarException {
+        ArrayList<Task> tasks = new ArrayList<>();
+
+        try {
+            File file = new File(filePath);
+            if (!file.exists()) {
+                return tasks;
+            }
+
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String input;
+            while ((input = reader.readLine()) != null) {
+                String[] inputs = input.split(" \\| ");
+
+                Task task;
+                switch (inputs[0]) {
+                case "T":
+                    task = new ToDo(inputs[2]);
+                    break;
+                case "D":
+                    task = new Deadline(inputs[2], inputs[3]);
+                    break;
+                case "E":
+                    task = new Event(inputs[2], inputs[3]);
+                    break;
+                default:
+                    throw new StarException("uh oh! there was an error during parsing: an unexpected task" +
+                            "type was found.");
+                }
+
+                if (Integer.parseInt(inputs[1]) == 1) {
+                    task.markDone();
+                }
+                tasks.add(task);
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new StarException("uh oh! the file of tasks couldn't be found :(");
+        } catch (IOException e) {
+            throw new StarException("uh oh! an IO exception occurred :(");
+        }
+        return tasks;
+    }
+
+    public void save(ArrayList<Task> tasklist) throws StarException {
+        try {
+            File fileDir = new File("./data");
+            if (!fileDir.exists()) {
+                fileDir.mkdirs();
+            }
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
+            for (Task task : tasklist) {
+                writer.append(task.getOutput());
+                writer.append("\n");
+            }
+
+        } catch (IOException e) {
+            throw new StarException("uh oh! an IO exception occurred :(");
+        }
+    }
 }
