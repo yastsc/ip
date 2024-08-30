28834a0a92d248ed3e7ba034baee6431838c81c1
yastsc <yaswitha.n04@gmail.com>
2024-08-30 15:38:45 +0800

    Add JavaDoc comments

--- src/main/java/star/main/TaskList.java
+++ src/main/java/star/main/TaskList.java
@@ -15,34 +15,67 @@ public class TaskList {
         this.tasks = tasks;
     }
 
+    /**
+     * Adds a task to the tasklist.
+     * @param task which is the task to add.
+     */
     public void addTask(Task task) {
         this.tasks.add(task);
     }
 
+    /**
+     * Removes a task with the specified index from the tasklist.
+     * @param index which is the index of the task to be removed.
+     */
     public void deleteTask(int index) {
         this.tasks.remove(index);
     }
 
+    /**
+     * Gets the task at the specified index from the tasklist.
+     * @param index which is the index of the task to retrieve.
+     * @return the task at the given index.
+     */
     public Task getTask(int index) {
         return this.tasks.get(index);
     }
 
+    /**
+     * Gets the number of tasks in the tasklist.
+     * @return the length and hence number of tasks in the tasklist.
+     */
     public int length() {
         return this.tasks.size();
     }
 
+    /**
+     * Checks whether the tasklist is empty or not.
+     * @return True if tasklist is empty and False otherwise.
+     */
     public boolean isEmpty() {
         return this.tasks.isEmpty();
     }
 
+    /**
+     * Returns the list of tasks.
+     * @return an ArrayList of all the tasks.
+     */
     public ArrayList<Task> getTasks() {
         return this.tasks;
     }
 
+    /**
+     * Marks the task at the specified index in the tasklist done.
+     * @param zeroIndex which is the index of the task to be marked done.
+     */
     public void markDone(int zeroIndex) {
         tasks.get(zeroIndex).markDone();
     }
 
+    /**
+     * Marks the task at the specified index in the tasklist undone.
+     * @param zeroIndex which is the index of the task to be marked undone.
+     */
     public void markUndone(int zeroIndex) {
         tasks.get(zeroIndex).markUndone();
     }
--- src/main/java/star/task/Task.java
+++ src/main/java/star/task/Task.java
@@ -10,24 +10,42 @@ public class Task {
         this.isDone = false;
     }
 
+    /**
+     * Gets status icon corresponding to status of task.
+     * @return a String of the icon.
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
+    /**
+     * Marks the task as done.
+     */
     public void markDone() {
         this.isDone = true;
 //        System.out.println("yay! this task is done: \n" + this);
     }
 
+    /**
+     * Marks the task as undone.
+     */
     public void markUndone() {
         this.isDone = false;
 //        System.out.println("oops it seems that this task isn't done yet: \n" + this);
     }
 
+    /**
+     * Gets the name of the task.
+     * @return a String of the description input, which is the name of the task.
+     */
     public String getDescription() {
         return this.description;
     }
 
+    /**
+     * Gets the output string of task to be saved in computer's hard drive.
+     * @return a String of the output.
+     */
     public String getOutput() {
         return String.format("X | %d | %s", isDone ? 1 : 0, description);
     }
