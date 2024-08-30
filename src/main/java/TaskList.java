94becda941c14e8cdf4970fb18a72bd66686ea65
yastsc <yaswitha.n04@gmail.com>
2024-08-30 11:34:05 +0800

    Add TaskList class

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,2 +1,45 @@
+import java.util.ArrayList;
+
 public class TaskList {
+    private ArrayList<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void addTask(Task task) {
+        this.tasks.add(task);
+    }
+
+    public void deleteTask(Task task) {
+        this.tasks.remove(task);
+    }
+
+    public Task getTask(int index) {
+        return this.tasks.get(index);
+    }
+
+    public int length() {
+        return this.tasks.size();
+    }
+
+    public boolean isEmpty() {
+        return this.tasks.isEmpty();
+    }
+
+    public ArrayList<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void markDone(int zeroIndex) {
+        tasks.get(zeroIndex).markDone();
+    }
+
+    public void markUndone(int zeroIndex) {
+        tasks.get(zeroIndex).markUndone();
+    }
 }
