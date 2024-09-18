aaad0e5bd306fb36551584c734a2a9efdf2e5363
yastsc <yaswitha.n04@gmail.com>
2024-09-19 04:05:58 +0800

    Update GUI to improve aesthetics

--- src/main/java/star/main/DialogBox.java
+++ src/main/java/star/main/DialogBox.java
@@ -46,6 +46,7 @@ public class DialogBox extends HBox {
         Collections.reverse(tmp);
         getChildren().setAll(tmp);
         setAlignment(Pos.TOP_LEFT);
+        dialog.getStyleClass().add("reply-label");
     }
 
     /**
--- src/main/java/star/main/Main.java
+++ src/main/java/star/main/Main.java
@@ -21,6 +21,8 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
             MainWindow controller = fxmlLoader.getController();
             controller.setStar(star); // inject the Star instance
             controller.showStarStartMessage();
